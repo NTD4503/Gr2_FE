2cf4bd20ff08ff8b2da3701ce7f938742dc5a547
NTD4503 <nguyendat12501@gmail.com>
2024-01-24 21:40:12 +0700

    CRUD event

---
+++ src/service/event.service.ts
@@ -0,0 +1,50 @@
+import APIConfig from "utils/APIConfig";
+import { POST } from "utils/url";
+
+export const createEventApi = (args: {
+    userId: string;
+    name: string;
+    description: string;
+    startTime: Date;
+    endTime: Date;
+}) => {
+    return POST({
+        url: APIConfig.CREATE_EVENT,
+        params: args,
+    }).then((data: any) => {
+        return data;
+    });
+};
+
+export const getAllEventByUserIdApi = (args: { userId: string }) => {
+    return POST({
+        url: APIConfig.GET_EVENT,
+        params: args,
+    }).then((data: any) => {
+        return data;
+    });
+};
+
+export const editEventByUserIdApi = (args: {
+    _id: string;
+    name: string;
+    description: string;
+    startTime: Date;
+    endTime: Date;
+}) => {
+    return POST({
+        url: APIConfig.EDIT_EVENT,
+        params: args,
+    }).then((data: any) => {
+        return data;
+    });
+};
+
+export const deleteEventByUserIdApi = (args: { _id: string }) => {
+    return POST({
+        url: APIConfig.DELETING_EVENT,
+        params: args,
+    }).then((data: any) => {
+        return data;
+    });
+};
